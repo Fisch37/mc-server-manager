46e8c577b95417f9ba81a102279c06adaa62e9d6
Fisch37 <seracin4@gmail.com>
2026-09-21 20:09:20 +0200

    Force change of ownership on each start
    this could be done with ACLs but somehow those are not a thing on ubuntu dockers?

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# Force ownership change
+chown 1000:1000 /var/mcsm
 su $(id -nu 1000) -c "java -jar app.jar" &
 nginx -g 'daemon off;' &
 tail -f /var/log/nginx/access.log &
 tail -f /var/log/nginx/error.log &
 wait -n
 exit $?
-# TODO: Attach output from /var/log/nginx/*
